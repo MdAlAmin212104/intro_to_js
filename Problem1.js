15ae937cf00d01ed99bce09fb0be212bbc2bfba1
MdAlAmin212104 <mdalamin212104@gmail.com>
2024-01-27 11:05:46 +0600

    javascript problem sovling

---
+++ Problem1.js
@@ -0,0 +1,31 @@
+// Task 1 of problem
+
+const myMoney = 1000;
+const oranges = 375 , apples = 325;
+const totalCost = oranges + apples;
+const moneyReturn = myMoney - totalCost;
+console.log(moneyReturn);
+
+
+// Task 2 of problem
+
+const Mathematics = 75.25,  Biology = 65, Chemistry = 80, Physics = 35.45, Bangla = 99.50;
+
+const allSubjectsTotalMarks = Mathematics + Biology + Chemistry + Physics + Bangla;
+
+const averageMarks = (allSubjectsTotalMarks / 5).toFixed(2);
+console.log(averageMarks);
+
+//Task 3 of problem
+
+const number = 119;
+const remainder = number % 5;
+console.log(remainder);
+
+
+// Task 4 of problem
+
+const number1 = 10;
+const number2 = "10";
+const result = number1 + number2;
+console.log(result);
